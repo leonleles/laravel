313227b87e2bd76d66ddd05c4da0e436e6a66a5a
leonleles <leoleles9159@hotmail.com>
2020-08-13 21:06:35 -0300

    Criando Models

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Contato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller {
@@ -11,6 +12,11 @@ class ContatoController extends Controller {
             (object)['nome' => "João", 'telefone' => '1111-1111'],
             (object)['nome' => "Joana", 'telefone' => '2222-2222'],
         ];
+
+        $contato = new Contato();
+        $con = $contato->index();
+        dd($con->nome);
+
         return view('contato.index', compact('contatos'));
     }
 
---
+++ app/Model/Contato.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contato extends Model
+{
+    public function index() {
+        return (object)[
+            'nome' => 'Leonardo Leles',
+            'tel' => '0000-0000'
+        ];
+    }
+}
